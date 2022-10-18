b5c99d448be68d44cc23a2e8b37abe2948aef9c0
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-17 22:34:20 -0400

    add font and background adjustments to css
    Co-authored-by: Bai  Jaitrong <bd2jaitrong@gmail.com>
    Co-authored-by: Grace Lee <gracexsnoopy@gmail.com>
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,43 +1,10 @@
 //Business Logic
 
-/*function getStolenBikes() {
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `https://bikeindex.org:443/api/v3/search?location=IP&distance=10&stolenness=stolen`;
-    request.addEventListener("loadend", function() {
-      const response = response.json();
-      if (this.status === 200) {
-        resolve(response);
-      } else {
-        reject(response);
-      }
-    });
-    request.open("GET", url, true);
-    request.send();
-  });
-  
-  promise.then(function(response) {
-    printElements(response);
-  }, function(errorMessage) {
-    printError(errorMessage);
-  });
-}*/
-
-// UI stuffs
-// function printElements(responseMessage) {
-//   document.getElementById("divOutput").innerHTML = responseMessage;
-// }
-
-// function printError(errorMessage) {
-//   document.getElementById("divOutput").innerHTML = errorMessage;
-// }
-
-//getStolenBikes();
-
 // Bike API: https://api.99spokes.com/docs
 //
 
 import './css/styles.css'
+import getBounty from './utility/Bounty';
 
 const stolenBikes = () => {
   const url = `https://bikeindex.org:443/api/v3/search?location=IP&distance=10&stolenness=stolen`;
@@ -57,7 +24,8 @@ const stolenBikes = () => {
           status: data.bikes[i].status,
           serial: data.bikes[i].serial,
           location: data.bikes[i].stolen_location,
-          image: data.bikes[i].large_img
+          image: data.bikes[i].large_img,
+          bounty: getBounty(data.bikes[i].manufacturer_name)
         };
         stolenBikes.push(stolenBike);
       }
@@ -79,5 +47,7 @@ const displayStolenBike = (stolenBikes) => {
   `).join('<br>');
   outputDiv.innerHTML = bikesHTMLString;
 };
+
+//function displayError {for bad API request}
 
 stolenBikes();
