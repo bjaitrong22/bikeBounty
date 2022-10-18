dbd23d707691967eb9892ef2853fbad6c5d192dd
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-17 22:29:12 -0400

    add basic functionality to assess bike bounty
    Co-authored-by: Grace Lee <gracexsnoopy@gmail.com>
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

---
+++ src/utility/bounty.js
@@ -0,0 +1,17 @@
+export default function getBounty(bikeManu) {
+  console.log('Bike Bounty');
+  console.log(bikeManu);
+  return 5.99;
+}
+
+
+
+
+/* Bike Tier List
+God: HUFFY / mongoose
+AA: Rad Power Bikes / Gazelle / Devinci / 
+A: Norco Bikes / Bianci / Specialized
+B: Giant / Trek / Cannondale / SCOTT / Privateer / raleigh
+C: Schwinn / Dynacraft / kent
+
+*/
